c2acd6b5ff976570e135cb1e1cd0f7a2b87aac35
Aioramu <31703899+Aioramu@users.noreply.github.com>
2019-03-24 09:57:34 +0300

    imageurl add

--- randomprog.py
+++ randomprog.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import json
 
 query=input("Enter value ")
+query.replace(" ","+")
 html= urlopen("https://www.avito.ru/moskva?s_trg=3&q="+str(query))
 bsObj= BeautifulSoup(html)
 #print(bsObj.prettify())
@@ -27,6 +28,12 @@ def getName():
     for namelist in name:
         names.append(namelist.span.get_text())
     return names
+def getHref():
+    refs=[]
+    href=bsObj.findAll("a",{"class":"item-description-title-link"})
+    for hreflist in href:
+        refs.append(namelist.span.get_text())
+    return refs
 def getArray():
     with open("data_file.json", mode='w', encoding='utf-8') as f:
         request={"Запрос":query}
@@ -34,29 +41,26 @@ def getArray():
     namespace=getName()
     pricelist=getPrice()
     placelist=getMetro()
+    imageurl=GetImageContent()
     count=-1
     for i in namespace:
         count=count+1
         name=namespace[count]
         price=pricelist[count]
         place=placelist[count]
+        image=imageurl[count]
+        image="https:"+image
         place = place.replace(u'\xa0', u' ')
-        entry={"name":name,"price":price,"place":place}
+        entry={"name":name,"price":price,"place":place,"url":image}
         print(entry)
         with open("data_file.json", mode='a',encoding='utf-8') as feedsjson:
             json.dump(entry,feedsjson,ensure_ascii=False)
     return entry
 def GetImageContent():
-    imagecontent=bsObj.findAll("img",{"class":"large-picture-img"})
-    output = [x["src"] for x in imagecontent]
+    out=[]
+    imagecontent=bsObj.findAll("ul",{"class":"item-slider-list js-item-slider-list"})
+    for image in imagecontent:
+        out.append(image.find("img",{"class":"large-picture-img"}))
+    output = [x["src"] for x in out]
     return output
-def Filewriting():
-    filename='index.html'
-    with open(filename,'w') as file_object:
-        file_object.write('<html lang="ru" dir="ltr"><head><meta charset="utf-8">')
-        file_object.write(str(GetContent()))
-        imageurl=GetImageContent()
-        for image in imageurl:
-            file_object.write('<img src ="https:'+str(image)+'"/>\n')
-        print("Запись в файл готова")
 print(getArray())
